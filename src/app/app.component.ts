1062fc79d65b5cc6185e7ccd9051f311f9a2dbdb
Ezekiel De Leon <ezekiel.de.leon@jci.com>
2023-04-05 08:34:56 -0700

    Using DI for services and understanding under the hood.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,56 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {COURSES} from '../db-data';
-import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  InjectionToken,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { COURSES } from "../db-data";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
+
+function coursesServiceProvided(http: HttpClient): CoursesService {
+  return new CoursesService(http);
+}
+
+export const COURSES_SERVICE = new InjectionToken<CoursesService>(
+  "COURSES_SERVICE"
+);
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [
+    {
+      provide: COURSES_SERVICE,
+      useFactory: coursesServiceProvided,
+      deps: [HttpClient],
+    },
+  ],
 })
 export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
 
-
-  courses = COURSES;
-
-  constructor() {
-
-  }
+  constructor(
+    @Inject(COURSES_SERVICE) private coursesService: CoursesService
+  ) {}
 
   ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
   }
 
-
-
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("Course Saved"));
+  }
 }
--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,52 +1,44 @@
 import {
-    AfterContentInit,
-    AfterViewInit,
-    Component,
-    ContentChildren,
-    ElementRef,
-    EventEmitter,
-    Input,
-    OnInit,
-    Output,
-    QueryList,
-    ViewEncapsulation
-} from '@angular/core';
-import {Course} from '../model/course';
-import {CourseImageComponent} from '../course-image/course-image.component';
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  OnInit,
+  Output,
+  QueryList,
+  ViewEncapsulation,
+} from "@angular/core";
+import { Course } from "../model/course";
+import { CourseImageComponent } from "../course-image/course-image.component";
+import { CoursesService } from "../services/courses.service";
+import { COURSES_SERVICE } from "../app.component";
 
 @Component({
-    selector: 'course-card',
-    templateUrl: './course-card.component.html',
-    styleUrls: ['./course-card.component.css']
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
 })
 export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
 
-    @Input()
-    course: Course;
+  @Input()
+  cardIndex: number;
 
-    @Input()
-    cardIndex: number;
+  @Output("courseChanged")
+  courseEmitter = new EventEmitter<Course>();
 
-    @Output('courseChanged')
-    courseEmitter = new EventEmitter<Course>();
+  constructor(
+    @Inject(COURSES_SERVICE) private coursesService: CoursesService
+  ) {}
 
+  ngOnInit() {}
 
-    constructor() {
-
-    }
-
-    ngOnInit() {
-
-    }
-
-
-    onSaveClicked(description:string) {
-
-        this.courseEmitter.emit({...this.course, description});
-
-    }
-
-
-
-
+  onSaveClicked(description: string) {
+    this.courseEmitter.emit({ ...this.course, description });
+  }
 }
